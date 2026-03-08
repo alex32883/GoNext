c7a35382d11dddf4ebf4d7cf0ad944425c2a35bc
alex32883 <alex32883@users.noreply.github.com>
2026-03-08 09:13:58 -0400

    Initial

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
+
+export default function RootLayout() {
+  return (
+    <PaperProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </PaperProvider>
+  );
+}
